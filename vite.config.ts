588c1619bcab59ac3a1a35c5343090cf3ee0791a
da-z <andreizinca@gmail.com>
2024-01-28 10:43:11 +0100

    chore: update

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    nodePolyfills({
-      include: ["path", "crypto", "os", "stream", "fs"],
-    }),
-  ],
-  build: {
-    rollupOptions: {
-      external: ["fs/promises"],
-    },
-  },
+  plugins: [react()],
 });
