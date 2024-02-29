cf6dc4c4c9e79de14f9c586ac8ebc1b3aa2bd2cd
da-z <andreizinca@gmail.com>
2024-02-29 17:38:05 +0100

    chore: clean

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-// import { overrideGlobalXHR } from "tauri-xhr";
-//
-// overrideGlobalXHR();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
